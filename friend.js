3b8f697e620900d197f83e7fe17805ac23f41b3f
Jaxin1996 <32075390+Jaxin1996@users.noreply.github.com>
2018-01-17 08:57:20 -0800

    Add files via upload

---
+++ friend.js
@@ -0,0 +1,19 @@
+var friendList = [
+    {
+      name: 'Jaxin San Filippo',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
